6b015c74ffefd78f90fbc7f72356420f920ffec2
Luke Imhoff <luke_imhoff@rapid7.com>
2014-04-25 07:43:34 -0500

    bundle gem yard-metasploit-erd
    MSP-9817

---
+++ lib/yard/metasploit/erd.rb
@@ -0,0 +1,9 @@
+require "yard/metasploit/erd/version"
+
+module Yard
+  module Metasploit
+    module Erd
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/yard/metasploit/erd/version.rb
@@ -0,0 +1,7 @@
+module Yard
+  module Metasploit
+    module Erd
+      VERSION = "0.0.1"
+    end
+  end
+end
