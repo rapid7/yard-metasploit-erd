ba4a5cde98b584d92de097faa12c7af19acb46fe
Luke Imhoff <luke_imhoff@rapid7.com>
2015-03-05 13:44:16 -0600

    require 'spec_helper' automatically
    MSP-12258

--- spec/yard/metasploit/erd/version_spec.rb
+++ spec/yard/metasploit/erd/version_spec.rb
@@ -1,5 +1,3 @@
-require 'spec_helper'
-
 require 'active_support/core_ext/object/blank'
 
 RSpec.describe YARD::Metasploit::ERD::Version do
--- spec/yard/metasploit/erd_spec.rb
+++ spec/yard/metasploit/erd_spec.rb
@@ -1,5 +1,3 @@
-require 'spec_helper'
-
 RSpec.describe YARD::Metasploit::ERD do
   context 'CONSTANTS' do
     context 'VERSION' do
