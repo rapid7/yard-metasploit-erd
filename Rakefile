ac0d32f49265d78806d7371f553ad5c0b307abc1
Luke Imhoff <luke_imhoff@rapid7.com>
2015-03-05 14:10:28 -0600

    Use metasploit-version
    MSP-11113

--- Rakefile
+++ Rakefile
@@ -1,9 +1,7 @@
 require 'bundler/gem_tasks'
+require 'bundler/setup'
 require 'rspec/core/rake_task'
-require 'yard'
-
-load 'tasks/yard.rake'
 
 RSpec::Core::RakeTask.new(:spec)
 
-task :default => :spec
+task default: :spec
--- lib/yard/metasploit/erd/version.rb
+++ lib/yard/metasploit/erd/version.rb
@@ -3,20 +3,32 @@ module YARD
     module ERD
       # Holds components of {VERSION} as defined by {http://semver.org/spec/v2.0.0.html semantic versioning v2.0.0}.
       module Version
+        #
+        # CONSTANTS
+        #
+
         # The major version number.
         MAJOR = 0
         # The minor version number, scoped to the {MAJOR} version number.
         MINOR = 0
-        # The patch number, scoped to the {MINOR} version number.
-        PATCH = 2
-        # The prerelease version, scoped to the {MAJOR}, {MINOR}, and {PATCH} version number.
-        PRERELEASE = 'rspec-3.1'
+        # The patch version number, scoped to the {MAJOR} and {MINOR} version numbers.
+        PATCH = 3
+        # The prerelease version, scoped to the {MAJOR}, {MINOR}, and {PATCH} version numbers.
+        PRERELEASE = 'metasploit-version'
 
-        # The full version string, including the {MAJOR}, {MINOR}, {PATCH}, and optionally, the {PRERELEASE} in the
+        #
+        # Module Methods
+        #
+
+        # The full version string, including the {YARD::Metasploit::ERD::Version::MAJOR},
+        # {YARD::Metasploit::ERD::Version::MINOR}, {YARD::Metasploit::ERD::Version::PATCH}, and optionally, the
+        # `YARD::Metasploit::ERD::Version::PRERELEASE` in the
         # {http://semver.org/spec/v2.0.0.html semantic versioning v2.0.0} format.
         #
-        # @return [String] '{MAJOR}.{MINOR}.{PATCH}' on master.  '{MAJOR}.{MINOR}.{PATCH}-{PRERELEASE}' on any branch
-        #   other than master.
+        # @return [String] '{YARD::Metasploit::ERD::Version::MAJOR}.{YARD::Metasploit::ERD::Version::MINOR}.
+        #   {YARD::Metasploit::ERD::Version::PATCH}' on master.
+        #   '{YARD::Metasploit::ERD::Version::MAJOR}.{YARD::Metasploit::ERD::Version::MINOR}.{YARD::Metasploit::ERD::Version::PATCH}-PRERELEASE'
+        #   on any branch other than master.
         def self.full
           version = "#{MAJOR}.#{MINOR}.#{PATCH}"
 
@@ -27,20 +39,23 @@ module YARD
           version
         end
 
-        # The full gem version string, including the {MAJOR}, {MINOR}, {PATCH}, and optionally, the {PRERELEASE} in the
+        # The full gem version string, including the {YARD::Metasploit::ERD::Version::MAJOR},
+        # {YARD::Metasploit::ERD::Version::MINOR}, {YARD::Metasploit::ERD::Version::PATCH}, and optionally, the
+        # `YARD::Metasploit::ERD::Version::PRERELEASE` in the
         # {http://guides.rubygems.org/specification-reference/#version RubyGems versioning} format.
         #
-        # @return [String] '{MAJOR}.{MINOR}.{PATCH}' on master.  '{MAJOR}.{MINOR}.{PATCH}.{PRERELEASE}' on any branch
-        #   other than master.
+        # @return [String] '{YARD::Metasploit::ERD::Version::MAJOR}.{YARD::Metasploit::ERD::Version::MINOR}.{YARD::Metasploit::ERD::Version::PATCH}'
+        #   on master.  '{YARD::Metasploit::ERD::Version::MAJOR}.{YARD::Metasploit::ERD::Version::MINOR}.{YARD::Metasploit::ERD::Version::PATCH}.PRERELEASE'
+        #   on any branch other than master.
         def self.gem
           full.gsub('-', '.pre.')
         end
       end
 
-      # @see Version.gem
+      # (see Version.gem)
       GEM_VERSION = Version.gem
 
-      # @see Version.full
+      # (see Version.full)
       VERSION = Version.full
     end
   end
